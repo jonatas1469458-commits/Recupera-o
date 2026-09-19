d05439196add67ec2c7d6340338759cc31276d9b
Jonataszk <jonatas1469458@gmail.com>
2026-09-19 02:45:22 -0300

    Maior numero.php
    Fix comparison logic to correctly identify the largest number.

---
+++ maior.php
@@ -0,0 +1,17 @@
+<?php
+
+$n1 = 20;
+$n2 = 15;
+$n3 = 3;
+
+if ($n1 > $n2 && $n1 > $n2){
+    echo "O maior numero $n1";
+}
+elseif ($n2 > $n1 && $n2 > $n3) {
+    echo "O maior número é: $n2";
+} else {
+    echo "O maior número é: $n3";
+}
+
+?>
+
